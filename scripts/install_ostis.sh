d13d85afb53640e5a4f297438ff2293523562a6a
Mikhail Kovalev <michail.kovalev217@gmail.com>
2018-04-04 11:33:17 +0300

    feat(scripts): Add check for updating or install platform

--- scripts/install_ostis.sh
+++ scripts/install_ostis.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/ShunkevichDV/ostis.git
-echo "../kb/" >> ../repo.path
-./ostis/scripts/prepare.sh
+if [ -d "ostis" ]; 
+	then
+		echo -en "Update OSTIS platform\n"
+		cd ostis
+		git pull
+		cd ../
+	else
+		echo -en "Install OSTIS platform\n"
+		git clone https://github.com/ShunkevichDV/ostis.git
+		echo "../kb" >> ostis/repo.path
+fi
+
+cd ostis/scripts
+./prepare.sh
