ea0c0fc2e1f85e85e121b5d7db23c88e81fd86cf
KovalM <michail.kovalev7@gmail.com>
2024-10-10 00:23:21 +0300

    build: update install scripts

--- scripts/install_py_sc_server_deps.sh
+++ scripts/install_py_sc_server_deps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 
+python3 -m venv "$APP_ROOT_PATH/.venv"
+source "$APP_ROOT_PATH/.venv/bin/activate"
 pip3 install -r "$APP_ROOT_PATH/problem-solver/py/requirements.txt"
