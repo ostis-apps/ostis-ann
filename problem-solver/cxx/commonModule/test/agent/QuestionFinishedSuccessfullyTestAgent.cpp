43b0348facf5c974ef536e23e939a0100310ea27
KovalM <michail.kovalev7@gmail.com>
2024-10-10 01:41:35 +0300

    build: upgrade to sc-machine without codegen

--- problem-solver/cxx/commonModule/test/agent/QuestionFinishedSuccessfullyTestAgent.cpp
+++ problem-solver/cxx/commonModule/test/agent/QuestionFinishedSuccessfullyTestAgent.cpp
@@ -1,30 +1,15 @@
-#include "sc-agents-common/utils/IteratorUtils.hpp"
-#include "sc-agents-common/utils/AgentUtils.hpp"
+#include "QuestionFinishedSuccessfullyTestAgent.hpp"
 
 #include "test/keynodes/TestKeynodes.hpp"
 
-#include "QuestionFinishedSuccessfullyTestAgent.hpp"
-
 using namespace commonTest;
 
-SC_AGENT_IMPLEMENTATION(QuestionFinishedSuccessfullyTestAgent)
+ScResult ActionFinishedSuccessfullyTestAgent::DoProgram(ScActionInitiatedEvent const & event, ScAction & action)
 {
-  if (!edgeAddr.IsValid())
-  {
-    return SC_RESULT_ERROR;
-  }
+  return action.FinishSuccessfully();
+}
 
-  ScAddr actionAddr = m_memoryCtx.GetEdgeTarget(edgeAddr);
-
-  ScIterator3Ptr iterator3Ptr = m_memoryCtx.Iterator3(
-        TestKeynodes::successfully_finished_test_action,
-        ScType::EdgeAccessConstPosPerm,
-        actionAddr);
-  if(!iterator3Ptr->Next())
-  {
-    return SC_RESULT_OK;
-  }
-
-  utils::AgentUtils::finishAgentWork(&m_memoryCtx, actionAddr, true);
-  return SC_RESULT_OK;
+ScAddr ActionFinishedSuccessfullyTestAgent::GetActionClass() const
+{
+  return TestKeynodes::successfully_finished_test_action;
 }
