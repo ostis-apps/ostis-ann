3b8089c62b97fd4b71d8764fd408906df3749371
robilkot <timur.robilko@gmail.com>
2024-11-07 16:29:45 +0300

    fix: revert server.py changes

--- problem-solver/py/server.py
+++ problem-solver/py/server.py
@@ -6,11 +6,9 @@ import sc_kpm
 from sc_client.client import create_elements
 from sc_client.constants import sc_types
 from sc_client.models import ScConstruction
-from sc_client.models import ScLinkContent, ScLinkContentType
 from sc_kpm import ScServer
 from modules.fnnProcessingModule.FnnInterpreterModule import FnnInterpreterModule
-from modules.fnnProcessingModule.FnnTrainingModule import FnnTrainingModule
-from pathlib import Path
+from modules.fnnProcessingModule.fnnTrainer.FnnTrainingModule import FnnTrainingModule
 
 SC_SERVER_PROTOCOL = "protocol"
 SC_SERVER_HOST = "host"
@@ -21,33 +19,16 @@ SC_SERVER_HOST_DEFAULT = "localhost"
 SC_SERVER_PORT_DEFAULT = "8090"
 
 
-def init_trainer():
-    time.sleep(1.5)
-
-    construction = ScConstruction()  # First you need initialize
-
-    question_initiated_addr = sc_kpm.ScKeynodes['question_initiated']
-    train_mnist_fashion_addr = sc_kpm.ScKeynodes['train_mnist_fashion']
-
-    construction.create_edge(sc_types.EDGE_ACCESS_CONST_POS_PERM, question_initiated_addr, train_mnist_fashion_addr)
-
-    addrs = create_elements(construction)
-
-
 def main(args: dict):
     server = ScServer(
         f"{args[SC_SERVER_PROTOCOL]}://{args[SC_SERVER_HOST]}:{args[SC_SERVER_PORT]}")
 
     with server.connect():
         modules = [
-            FnnInterpreterModule(),
-            FnnTrainingModule()
+            FnnAgentProcessingModule(),
+            FnnTrainerProcessingModule()
         ]
         server.add_modules(*modules)
-
-        thread = Thread(target=init_trainer)
-        thread.start()
-
         with server.register_modules():
             server.serve()
 
