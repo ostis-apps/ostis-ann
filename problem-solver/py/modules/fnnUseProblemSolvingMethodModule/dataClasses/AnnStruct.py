d5136bad33f0c407d7e6aebea604824043b6a4a1
DanTen317 <daniil.317.d@gmail.com>
2024-12-07 15:03:23 +0300

    fix(solver): update AnnStruct class

--- problem-solver/py/modules/fnnUseProblemSolvingMethodModule/dataClasses/AnnStruct.py
+++ problem-solver/py/modules/fnnUseProblemSolvingMethodModule/dataClasses/AnnStruct.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 from typing import List
 
+import keras
 from sc_client.models import ScAddr
 
 
 @dataclass
 class AnnStruct:
     network_address: ScAddr
+    ann_model: keras.Model
     num_of_inputs: int
     num_of_outputs: int
     output_labels: List[str] = None
