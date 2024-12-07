20dbe35be57cd5d1bedb11ccf9335d98c1e59978
DanTen317 <daniil.317.d@gmail.com>
2024-12-07 15:19:49 +0300

    feat(solver): add UseProblemSolvingMethodAgent run

--- problem-solver/py/modules/fnnUseProblemSolvingMethodModule/converters/ImageConverter.py
+++ problem-solver/py/modules/fnnUseProblemSolvingMethodModule/converters/ImageConverter.py
@@ -5,8 +5,9 @@ from modules.fnnUseProblemSolvingMethodModule.dataClasses.inputOutput.ImageStruc
 
 
 class ImageConverter(Converter):
+    @staticmethod
     def convert_image_to_inputs(image:Image, number_of_inputs:int) -> numpy.array:
-        numpydata = image.image_array
+        numpydata = image.data_array
         numpy.shape(numpydata)
 
         numpydata = numpy.reshape(numpydata, (1, number_of_inputs))
