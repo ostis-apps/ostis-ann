4544a39d499267d44b52739ecfed6c6e0858febb
DanTen317 <daniil.317.d@gmail.com>
2024-12-08 15:38:22 +0300

    refactor(solver): old FnnUseProblemSolvingMethodAgent deleted

--- problem-solver/py/modules/fnnUseProblemSolvingMethodModule/FnnUseProblemSolvingMethodModule.py
+++
@@ -1,8 +0,0 @@
-from sc_kpm import ScModule
-
-from .FnnUseProblemSolvingMethodAgent import FnnUseProblemSolvingMethodAgent
-
-
-class FnnUseProblemSolvingMethodModule(ScModule):
-    def __init__(self) -> None:
-        super().__init__(FnnUseProblemSolvingMethodAgent())
