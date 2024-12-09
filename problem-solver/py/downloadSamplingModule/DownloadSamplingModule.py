4c8ef002c1c4e0fbb7c5926a5ae1ed1ce29a5171
stacey__:) <117319752+staceeey89@users.noreply.github.com>
2024-12-09 13:52:59 +0300

    Delete problem-solver/py/downloadSamplingModule directory

--- problem-solver/py/downloadSamplingModule/DownloadSamplingModule.py
+++
@@ -1,7 +0,0 @@
-from sc_kpm import ScModule
-from modules.downloadSamplingModule.DownloadSamplingAgent import DownloadSamplingSolver
-
-
-class DownloadSamplingModule (ScModule):
-    def __init__(self):
-        super().__init__(DownloadSamplingSolver()) 
