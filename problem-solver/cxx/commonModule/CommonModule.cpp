0d82adf370e4786a1ad1025041ab36157b18091e
ToxicVafla <vasilikruk235@gmail.com>
2024-12-10 01:09:50 +0300

    feat: implement recommend training parameters agent

--- problem-solver/cxx/commonModule/CommonModule.cpp
+++ problem-solver/cxx/commonModule/CommonModule.cpp
@@ -1,7 +1,10 @@
 #include "CommonModule.hpp"
 
 #include "agent/NonAtomicActionInterpreterAgent.hpp"
+#include "agent/sc_agent_recommend_training_parameters.hpp"
 
 using namespace commonModule;
 
-SC_MODULE_REGISTER(CommonModule)->Agent<NonAtomicActionInterpreterAgent>();
+SC_MODULE_REGISTER(CommonModule)
+  ->Agent<NonAtomicActionInterpreterAgent>()
+  ->Agent<RecommendTrainingParametersAgent>();
